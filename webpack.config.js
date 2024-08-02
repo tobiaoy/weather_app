f1eb09522fc21425de1f2a912c3c0d0dfaa71579
Tobi Oyero <tobioyero@tobis-mbp-2.home>
2024-08-02 11:37:42 -0400

    fixed html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,60 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const path = require("path");
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import { resolve as _resolve, join } from "path";
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './src/app.js'),
-      },
-    
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'deploy'),
-        clean: true
-      },
-    
-      devServer: {
-        static: [
-            { directory: path.join(__dirname, 'deploy') },
-            // { directory: path.join(__dirname, 'src') }
-        ],
-        port: 3000,
-        open: true,
-        hot: true
-    },
-    
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-
-            {
-                test: /\.css/,
-                use: ["style-loader", "css-loader"]
-            },
-
-            {
-                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][ext][query]', 
-                },
-            }
-        ]
-    },  
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Weather App",
-            template: path.resolve(__dirname, 'src/template.html')
-        }),
-
-        new CleanWebpackPlugin()
+export const mode = 'production';
+export const entry = {
+    main: _resolve(__dirname, './src/app.js'),
+};
+export const output = {
+    filename: '[name].bundle.js',
+    path: _resolve(__dirname, 'deploy'),
+    clean: true
+};
+export const devServer = {
+    static: [
+        { directory: join(__dirname, 'deploy') },
     ],
-    
-    resolve: {
-        extensions: ['.js', '.json'],
-    },
+    port: 3000,
+    open: true,
+    hot: true
+};
+export const module = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
 
-    devtool: 'source-map'
-}
+        {
+            test: /\.css/,
+            use: ["style-loader", "css-loader"]
+        },
+
+        {
+            test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'assets/[name][ext][query]',
+            },
+        }
+    ]
+};
+export const plugins = [
+    new HtmlWebpackPlugin({
+        title: "Weather App",
+        template: _resolve(__dirname, 'src/template.html')
+    }),
+
+    new CleanWebpackPlugin()
+];
+export const resolve = {
+    extensions: ['.js', '.json'],
+};
+export const devtool = 'source-map';
